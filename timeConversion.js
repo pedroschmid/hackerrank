9f2fcdc9b4d8a6bdf0887b32b30d5c7b61f9230e
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-02-20 15:48:25 -0300

    Added missing function formatTime in timeConversion.js

--- timeConversion.js
+++ timeConversion.js
@@ -1,5 +1,13 @@
 // https://www.hackerrank.com/challenges/time-conversion/problem
 
+function formatTime(time, isPM = false) {
+  if (isPM && time !== "12") return parseInt(time) + 12;
+  if (isPM && time === "12") return "12";
+  if (!isPM && time === "12") return "00";
+
+  return time.replace(/[^0-9]/g, "");
+}
+
 function timeConversion(s) {
   const string = s;
   const isPM = string.match("PM");
@@ -11,6 +19,7 @@ function timeConversion(s) {
           index === 0 ? formatTime(item, true) : formatTime(item)
         )
         .join(":")
+        
     : string
         .split(":")
         .map((item, index) =>
