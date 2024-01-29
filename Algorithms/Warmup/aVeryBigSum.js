11cb31d720c422e863cdf954347fc1784b9b24ed
Pedro Abreu <pedro.schmid2311@gmail.com>
2024-01-29 11:03:13 -0300

    remove javascript challenges

--- Algorithms/Warmup/aVeryBigSum.js
+++
@@ -1,5 +0,0 @@
-// https://www.hackerrank.com/challenges/a-very-big-sum/problem
-
-function aVeryBigSum(ar) {
-    return ar.reduce((a, b) => a + b)
-}
--- Algorithms/Warmup/simpleArraySum.js
+++
@@ -1,5 +0,0 @@
-// https://www.hackerrank.com/challenges/simple-array-sum/problem
-
-function simpleArraySum(ar) {
-    return ar.reduce((a, b) => a + b)
-}
